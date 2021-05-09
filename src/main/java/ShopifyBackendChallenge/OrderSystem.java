962856eb0499be45dd1a3f92f6993374c0c27fb1
Walter Wong <walterwong2000@gmail.com>
2021-05-09 14:41:27 -0600

    created skeleton code for OrderSystem to faciliate writing test cases

---
+++ src/main/java/ShopifyBackendChallenge/OrderSystem.java
@@ -0,0 +1,50 @@
+package ShopifyBackendChallenge;
+
+import ShopifyBackendChallenge.ImageInfo;
+import ShopifyBackendChallenge.ImageRepository;
+import java.util.HashMap;
+
+public class OrderSystem {
+    private HashMap<ImageInfo, Long> shoppingCart = new HashMap<>();
+    private String user;
+    private float money;
+    private float orderCost = 0;
+
+    // an expansion to the system would be to create an User object that tracks things like
+    // the amount of money this user has, previous purchases, login details, etc.
+    public OrderSystem(String user, float money) {
+        this.user = user;
+        this.money = money;
+    }
+
+    public boolean addItem(ImageInfo img, long quantity) {
+        return false; //dummy value for now
+    }
+
+    public boolean removeItem(ImageInfo img) {
+        return false;
+    }
+
+    public boolean removeItem(ImageInfo img, long quantity) {
+        return false;
+    }
+
+    public boolean checkout() {
+        // TODO: calculate total order price, including tax
+        return false;
+    }
+
+    public boolean checkout(float discount) {
+        // TODO: calculate total order price, including discount and tax
+        return false;
+    }
+
+    private boolean calculateDiscount(float discount) {
+        return false;
+    }
+
+    // Possible improvement: support different areas and their taxation
+    private boolean calculateTax() {
+        return false;
+    }
+}
