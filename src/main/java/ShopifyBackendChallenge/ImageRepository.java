dbcdc980fb5ff63a4b2ecdf564408dcf8522ac88
Walter Wong <walterwong2000@gmail.com>
2021-05-09 14:02:18 -0600

    created basic test cases for ImageRepository, and created methods for ImageRepository

--- src/main/java/ShopifyBackendChallenge/ImageRepository.java
+++ src/main/java/ShopifyBackendChallenge/ImageRepository.java
@@ -3,24 +3,117 @@ package ShopifyBackendChallenge;
 import java.util.*;
 
 public class ImageRepository {
-    public static final int DSIZE = 32;
-
-    private final int capacity;
-    private HashMap<ImageInfo, Long> stock;
+    private HashMap<ImageInfo, Long> stock = new HashMap<>();
 
     /**
-     * Constructor method to create image repository of a certain size.
-     * @param capacity size of the repository
+     * Constructor method to create image repository.
      */
-    public ImageRepository(int capacity) {
-        this.capacity = capacity;
+    public ImageRepository() {}
+
+    // TODO: add multithreading capability?
+    //  order system class to calculate discount
+
+    /**
+     * Adds an image item to the store/image repository
+     * @param img the image along with its details, to be added
+     * @param quantity the number of stock of this image
+     * @return boolean indicating the success(true)/failure(false) of this operation
+     */
+    public boolean addImage(ImageInfo img, long quantity) {
+        // checks to see if the image is already in the "store"
+        if (stock.containsKey(img)) {
+            return false;
+        }
+
+        stock.put(img, quantity);
+
+        // checks to see if the image was successfully added to the "store"
+        if (stock.containsKey(img)) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
-     * Creates the image repository with default size.
+     * Removes an image item from the store/image repository
+     * @param img the image and its details, to be removed
+     * @return boolean indicating the success(true)/failure(false) of this operation
      */
-    public ImageRepository() { this(DSIZE); }
+    public boolean removeImage(ImageInfo img) {
+        // checks to see if the image is in the stock
+        if (!stock.containsKey(img)) {
+            return false;
+        }
 
-    // TODO: add functions to add/remove images, buy images, check if the image is in-stock, add stock(quantity),
-    //  calculate discount, find most recent addition
+        stock.remove(img);
+
+        // checks to see if the image was successfully removed
+        if (!stock.containsKey(img)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Allows user to buy a specified quantity of an image
+     * @param img the image to be bought
+     * @param quantity the number of images to be bought
+     * @return boolean indicating the success(true)/failure(false) of this operation
+     */
+    public boolean buyImage(ImageInfo img, long quantity) {
+        if (inStock(img, quantity)) {
+            stock.put(img, stock.get(img) - quantity);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Helper method to check if the image is in stock, and in sufficient quantity
+     * @param img the image to be checked
+     * @param quantity the minimum quantity needed
+     * @return boolean indicating the success(true)/failure(false) of this operation
+     */
+    private boolean inStock(ImageInfo img, long quantity) {
+        if (!stock.containsKey(img)) {
+            return false;
+        }
+        else {
+            if (stock.get(img) < quantity) {
+                long deficiency = quantity - stock.get(img);
+                System.out.println("Insufficient stock, missing " + deficiency + " unit(s).");
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    /**
+     * Adds stock for an image that already exists in the store/image repository
+     * @param img the image in question
+     * @param quantity the amount of stock to be added
+     * @return boolean indicating the success(true)/failure(false) of this operation
+     */
+    public boolean addStock(ImageInfo img, long quantity) {
+        if (!stock.containsKey(img)) {
+            return false;
+        }
+
+        stock.put(img, stock.get(img) + quantity);
+        // there's no need to check that the image was added successfully here, sufficient trust in Java's HashMap functions
+        return true;
+    }
+
+    public long getQuantity(ImageInfo img) {
+        if (!stock.containsKey(img)) {
+            return -1; //consider if this is a magic number?
+        }
+
+        return stock.get(img);
+    }
 }
--- src/test/java/ShopifyBackendChallengeTests/ImageRepositoryTests.java
+++ src/test/java/ShopifyBackendChallengeTests/ImageRepositoryTests.java
@@ -27,7 +27,70 @@ public class ImageRepositoryTests {
         String category = "Sports";
         ImageInfo image1 = new ImageInfo(img, title, desc, price, rating, category);
         ImageRepository store = new ImageRepository();
-//        store.addImage(image1);
-//        assertEquals(image1, store.getMostRecent());
+        store.addImage(image1, 1);
+        assertEquals(1, store.getQuantity(image1), 0.00001);
+    }
+
+    @Test
+    public void RemoveImageTest() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 3);
+        assertEquals(3, store.getQuantity(image1), 0.00001);
+        assertTrue(store.removeImage(image1));
+    }
+
+    @Test
+    public void BuyImageTest() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 3);
+        assertTrue(store.buyImage(image1, 2));
+        assertEquals(1, store.getQuantity(image1));
+    }
+
+    @Test
+    public void BuyImageTest2() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 3);
+        assertFalse(store.buyImage(image1, 4));
+    }
+
+    @Test
+    public void AddStockTest() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 3);
+        store.addStock(image1, 3);
+        assertEquals(6, store.getQuantity(image1));
     }
 }
