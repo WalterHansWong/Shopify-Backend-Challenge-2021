3280f10ec432f083ed47c2063b224f3611eb264b
Walter Wong <walterwong2000@gmail.com>
2021-05-09 14:20:46 -0600

    added more comphrehensive ImageRepository test cases, reaching 100% class and method coverage, 93% line coverage

--- src/test/java/ShopifyBackendChallengeTests/ImageRepositoryTests.java
+++ src/test/java/ShopifyBackendChallengeTests/ImageRepositoryTests.java
@@ -32,6 +32,62 @@ public class ImageRepositoryTests {
     }
 
     @Test
+    public void AddImageTest2() {
+        BufferedImage img = null, img2 = null, img3 = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+            img2 = ImageIO.read(new File("./src/Images/swimming.jpg"));
+            img3 = ImageIO.read(new File("./src/Images/book.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet", title2 = "Michael Phelps", title3 = "Stack of Books";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageInfo image2 = new ImageInfo(img2, title2);
+        ImageInfo image3 = new ImageInfo(img3, title3);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 1);
+        store.addImage(image2, 2);
+        assertEquals(1, store.getQuantity(image1), 0.00001);
+        assertEquals(2, store.getQuantity(image2), 0.00001);
+    }
+
+    @Test
+    public void AddImageTest3() {
+        BufferedImage img = null, img2 = null, img3 = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+            img2 = ImageIO.read(new File("./src/Images/swimming.jpg"));
+            img3 = ImageIO.read(new File("./src/Images/book.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet", title2 = "Michael Phelps", title3 = "Stack of Books";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageInfo image2 = new ImageInfo(img2, title2);
+        ImageInfo image3 = new ImageInfo(img3, title3);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 1);
+        store.addImage(image2, 2);
+        assertEquals(-1, store.getQuantity(image3), 0.00001);
+    }
+
+    @Test
+    public void AddImageTest4() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        store.addImage(image1, 1);
+        assertFalse(store.addImage(image1, 1));
+    }
+
+    @Test
     public void RemoveImageTest() {
         BufferedImage img = null;
         try {
@@ -45,6 +101,20 @@ public class ImageRepositoryTests {
         store.addImage(image1, 3);
         assertEquals(3, store.getQuantity(image1), 0.00001);
         assertTrue(store.removeImage(image1));
+    }
+
+    @Test
+    public void RemoveImageTest2() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        assertFalse(store.removeImage(image1));
     }
 
     @Test
@@ -79,6 +149,20 @@ public class ImageRepositoryTests {
     }
 
     @Test
+    public void BuyImageTest3() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        assertFalse(store.buyImage(image1, 4));
+    }
+
+    @Test
     public void AddStockTest() {
         BufferedImage img = null;
         try {
@@ -92,5 +176,19 @@ public class ImageRepositoryTests {
         store.addImage(image1, 3);
         store.addStock(image1, 3);
         assertEquals(6, store.getQuantity(image1));
+    }
+
+    @Test
+    public void AddStockTest2() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("./src/Images/badminton.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String title = "Badminton Racquet";
+        ImageInfo image1 = new ImageInfo(img, title);
+        ImageRepository store = new ImageRepository();
+        assertFalse(store.addStock(image1, 3));
     }
 }
